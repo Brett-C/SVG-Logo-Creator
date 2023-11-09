afd7288d9803a43425465887a0516ada7aec170d
Brett C <brcannon@bellsouth.net>
2023-11-08 19:01:59 -0600

    updated README file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # SVG-Logo-Creator
+
+## Description
+
+This is a simple console command line application that will take in 3 arguments and create a small logo in SVG format based on what the user puts in.
+
+## Table of Contents
+
+none
+
+## Installation
+
+Simply clone the repo into your local system and open the folder in vs code
+
+## Usage
+
+To start the application simply npm install in the folder and then run 'node index.js'. Follow the command prompts and it will output an SVG file based on the criteria that was put in. 
+
+## Credits
+
+Resources that were used were:
+
+    inquirer
+    
+    node.js
+
+    JavaScript
--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
-const {Circle, Square, Triangle} = require('./lib/shapes');
+const { Circle, Square, Triangle } = require('./lib/shapes');
 
 const { default: generate } = require('@babel/generator');
+const { validate } = require('@babel/types');
 
 const shapeQuestion = 'What shape would you like your logo to be?'
 const shapeColorQuestion = 'What color would you like your shape to be? If using a hex number, remember to include the "#"'
 const textQuestion = 'What  letters would you like to use for your svg logo? Please enter up to 3 leters'
 const textColorQuestion = 'What color would you like your text to be? If using a hex number, remember to include the "#"'
 
+function validateLength(input) {
+    if (input.length !== 3) {
+        return "Characters must contain 3 letters."
+    }
+    return true;
+};
 
+function validateAnswer(input) {
+    if (input == "") {
+        return "Choice can not be empty."
+    }
+    return true;
+}
 function logoCreator() {
     inquirer.prompt([
         {
@@ -26,37 +39,44 @@ function logoCreator() {
             name: 'shape_color',
             type: 'input',
             message: shapeColorQuestion,
+            validate: validateAnswer
         },
         {
             name: 'characters',
             type: 'input',
             message: textQuestion,
+            validate: validateLength
         },
         {
             name: 'text_color',
             type: 'input',
             message: textColorQuestion,
+            validate: validateAnswer
         }
 
     ])
 
         .then((answer) => {
-            console.log(answer.shape)
-            console.log(answer.shape_color)
-            console.log(answer.characters)
-            console.log(answer.text_color)
-            let circle = new Circle(answer.shape_color, answer.characters, answer.text_color)
-            let square = new Square(answer.shape_color, answer.characters, answer.text_color)
-            let triangle = new Triangle(answer.shape_color, answer.characters, answer.text_color)
-            circle.generate();
-            square.generate();
-            triangle.generate();
-            console.log(circle.generate())
-            console.log(square.generate())
-            console.log(triangle.generate())
+            const shapes = {
+                Circle: Circle,
+                Square: Square,
+                Triangle: Triangle
+            }
+            const chosenShapeClass = shapes[answer.shape];
+
+            const shape = new chosenShapeClass(answer.shape_color, answer.characters, answer.text_color);
+
+            writeToFile("./example/logo.svg", shape.render());
         })
 
+
+
 }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        console.log('no errors')
+    })
+};
 
 
 logoCreator();
